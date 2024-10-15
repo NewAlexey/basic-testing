f0fc12db68a1429d299648160e6796a8e97ee2b9
NewAlexey <alexey.krupenia@gmail.com>
2024-10-15 15:38:05 +0300

    feat: implement 1, 2, 3 and 4 test tasks

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,42 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 2, b: 3, action: Action.Add });
+    expect(result).toEqual(5);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 5, b: 3, action: Action.Subtract });
+    expect(result).toEqual(2);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 5, b: 3, action: Action.Multiply });
+    expect(result).toEqual(15);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 27, b: 3, action: Action.Divide });
+    expect(result).toEqual(9);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({
+      a: 2,
+      b: 3,
+      action: Action.Exponentiate,
+    });
+    expect(result).toEqual(8);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 5, b: 3, action: '42' });
+    expect(result).toEqual(null);
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 15, b: '25', action: Action.Divide });
+    expect(result).toEqual(null);
   });
 });
--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,44 +1,129 @@
-// Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import {
+  getBankAccount,
+  InsufficientFundsError,
+  SynchronizationFailedError,
+  TransferFailedError,
+} from './index';
 
 describe('BankAccount', () => {
   test('should create account with initial balance', () => {
-    // Write your test here
+    const balance = 42;
+
+    const account = getBankAccount(balance);
+    expect(account.getBalance()).toEqual(balance);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    const balance = 42;
+    const amount = 420;
+
+    const account = getBankAccount(balance);
+
+    expect(() => account.withdraw(amount)).toThrow(InsufficientFundsError);
   });
 
   test('should throw error when transferring more than balance', () => {
-    // Write your test here
+    const balance = 42;
+    const otherBalance = 420;
+
+    const account = getBankAccount(balance);
+    const otherAccount = getBankAccount(otherBalance);
+
+    expect(() => account.transfer(balance + 10, otherAccount)).toThrow(
+      InsufficientFundsError,
+    );
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    const balance = 42;
+
+    const account = getBankAccount(balance);
+
+    expect(() => account.transfer(balance + 10, account)).toThrow(
+      TransferFailedError,
+    );
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    const balance = 42;
+    const deposit = 15;
+    const resultDeposit = balance + deposit;
+
+    const account = getBankAccount(balance);
+    jest.spyOn(account, 'deposit');
+    account.deposit(deposit);
+
+    expect(account.getBalance()).toEqual(resultDeposit);
+    expect(account.deposit).toHaveBeenCalled();
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    const balance = 42;
+    const deposit = 15;
+    const resultDeposit = balance - deposit;
+
+    const account = getBankAccount(balance);
+    jest.spyOn(account, 'withdraw');
+    account.withdraw(deposit);
+
+    expect(account.getBalance()).toEqual(resultDeposit);
+    expect(account.withdraw).toHaveBeenCalled();
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    const balance = 42;
+    const otherBalance = 20;
+    const transferAmount = 10;
+
+    const accountBalance = balance - transferAmount;
+    const otherAccountBalance = otherBalance + transferAmount;
+
+    const account = getBankAccount(balance);
+    const otherAccount = getBankAccount(otherBalance);
+    jest.spyOn(account, 'transfer');
+    jest.spyOn(account, 'withdraw');
+    jest.spyOn(otherAccount, 'deposit');
+    account.transfer(transferAmount, otherAccount);
+
+    expect(account.getBalance()).toEqual(accountBalance);
+    expect(otherAccount.getBalance()).toEqual(otherAccountBalance);
+    expect(account.transfer).toHaveBeenCalled();
+    expect(account.withdraw).toHaveBeenCalled();
+    expect(otherAccount.deposit).toHaveBeenCalled();
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
-    // Write your tests here
+    const newBalance = 42;
+
+    const account = getBankAccount(5);
+    jest
+      .spyOn(account, 'fetchBalance')
+      .mockImplementation(async () => newBalance);
+
+    const result = await account.fetchBalance();
+
+    expect(result).toEqual(newBalance);
   });
 
   test('should set new balance if fetchBalance returned number', async () => {
-    // Write your tests here
+    const newBalance = 42;
+
+    const account = getBankAccount(5);
+    jest
+      .spyOn(account, 'fetchBalance')
+      .mockImplementation(async () => newBalance);
+
+    await account.synchronizeBalance();
+
+    expect(account.getBalance()).toEqual(newBalance);
   });
 
   test('should throw SynchronizationFailedError if fetchBalance returned null', async () => {
-    // Write your tests here
+    const account = getBankAccount(5);
+    jest.spyOn(account, 'fetchBalance').mockImplementation(async () => null);
+
+    await expect(() => account.synchronizeBalance()).rejects.toThrow(
+      SynchronizationFailedError,
+    );
   });
 });
