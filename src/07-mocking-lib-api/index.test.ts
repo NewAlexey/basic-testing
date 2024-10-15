b309cd46319395de3f14d5213c5fadcada860a78
NewAlexey <alexey.krupenia@gmail.com>
2024-10-15 18:03:26 +0300

    feat: 7-th task

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,60 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios, { AxiosInstance } from 'axios';
+
+import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.runAllTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const baseURL = 'https://jsonplaceholder.typicode.com';
+
+    jest
+      .spyOn(axios, 'create')
+      .mockReturnValue({ get: () => jest.fn() } as unknown as AxiosInstance);
+    jest.spyOn(axios, 'get').mockReturnValue(Promise.resolve(undefined));
+
+    await throttledGetDataFromApi('some-path');
+
+    expect(axios.create).toHaveBeenCalledWith({ baseURL });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const providedURL = 'some-url';
+
+    const axiosInstance = {
+      get: jest.fn(),
+    } as unknown as AxiosInstance;
+
+    jest.spyOn(axios, 'create').mockReturnValue(axiosInstance);
+    jest
+      .spyOn(axiosInstance, 'get')
+      .mockReturnValue(Promise.resolve({ data: '' }));
+
+    await throttledGetDataFromApi(providedURL);
+
+    expect(axiosInstance.get).toHaveBeenCalledWith(providedURL);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const mockData: string[] = [];
+
+    const axiosInstance = {
+      get: jest.fn(),
+    } as unknown as AxiosInstance;
+
+    jest.spyOn(axios, 'create').mockReturnValue(axiosInstance);
+    jest
+      .spyOn(axiosInstance, 'get')
+      .mockReturnValue(Promise.resolve({ data: mockData }));
+
+    const result = await throttledGetDataFromApi('');
+
+    expect(result).toEqual(mockData);
   });
 });
